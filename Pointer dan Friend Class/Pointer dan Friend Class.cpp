e77e3860c8858c0ac0874cb63b043f1b11f7445e
MzSuwawa <yudharamadhan838@gmail.com>
2024-05-06 14:55:17 +0700

    Membuat prosedur utama

--- Pointer dan Friend Class/Pointer dan Friend Class.cpp
+++ Pointer dan Friend Class/Pointer dan Friend Class.cpp
@@ -8,3 +8,18 @@ public:
 		cout << "No Induk = " << nim << endl;
 	}
 };
+
+int main() {
+	
+	mahasiswa mhs{ 1 }; //object mhs
+	mhs.showNim(); //member access operator
+
+	mahasiswa &refMhs = mhs; //Pointer reference refMhs
+	refMhs.nim = 2; //Member access operator
+	mhs.showNim();
+
+	mahasiswa *pMhs = &mhs;
+	pMhs->nim = 3;
+	pMhs->showNim();
+	return 0;
+}
